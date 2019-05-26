c5f9275e8bf56f0f3ebc40c0027fee1a3da985f3
TomSterQaz <exln.tomsterqaz@gmail.com>
2019-05-26 10:57:21 -0500

    Removed JSON file logging Added clear command

--- PointBlank.py
+++ PointBlank.py
@@ -1,11 +1,9 @@
 import discord
-import json
 import random
+import json
 from discord.ext import commands
 
 client = commands.Bot(command_prefix = '>')
-
-print(discord.__version__)
 
 @client.event
 async def on_ready():
@@ -16,20 +14,13 @@ async def ping(ctx):
     await ctx.send('Ping: {:0.2f} ms'.format(client.latency * 1000))
 
 @client.command()
-async def get_msg(ctx, num):
-    with open('DataFiles/Test.JSON', 'r') as messages:
-        for item in messages[0:int(num)]:
-            await ctx.send(item)
-
-@client.command()
 async def coinflip(ctx):
     choice = random.choice(['Heads', 'Tails'])
     await ctx.send("It's {}!".format(choice))
 
 @client.command()
-async def quit(ctx):
-    pass
-
+async def clear(ctx, amount = 3):
+    await ctx.channel.purge(limit = amount)
 
 
 client.run('NTIxODYyMTI2NzQzMDYwNDkx.XMc4Dw.KRT39YgXQlXbB2Pd8FsAJrqwHKU')
